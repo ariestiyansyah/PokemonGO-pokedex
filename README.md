51702951b1a328ec2ce26d89f9ff5debdeb1a4a2
ariestiyansyah <ariestiyansyah.rizky@gmail.com>
2016-07-14 01:26:49 +0700

    update README, fix setup.py error

---
+++ README.md
@@ -0,0 +1,14 @@
+# PokemonGO-pokedex
+
+Get Pokemon name based on pokedex number in Pokemon GO
+
+#### Installation:
+`$ pip install PokemonGO-pokedex`
+
+### How to use:
+
+Run this command:
+
+`$ PokemonGO-pokedex`
+
+Enter Pokedex number.
--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import find_packages
 
 setup(
     name='PokemonGO-pokedex',
-    version='1.0.0',
+    version='1.0.1',
     scripts=['PokemonGO-pokedex'],
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Get Pokemon GO name based on Pokedex number',
     long_description='Get Pokemon GO name based on Pokedex number',
-    install_requires=['requests','json'],
+    install_requires=['requests'],
     url='http://github.com/ariestiyansyah/PokemonGO-pokedex',
     author='Rizky Ariestiyansyah',
     author_email='ariestiyansyah.rizky@gmail.com'
